9732ff6ab9d4898a20f8b7c4da5a4d1cd71ec2db
Pedro Augusto <pa886472@gmail.com>
2023-09-23 17:33:10 -0300

    Text Colors.py

---
+++ Text Colors.py
@@ -0,0 +1,18 @@
+print('\033[0;30mHello, World!\033[m') #Style: 0 / Text: 30 / Back: -
+print('\033[0;31mHello, World!\033[m') #Style: 0 / Text: 31 / Back: -
+print('\033[1;32mHello, World!\033[m') #Style: 1 / Text: 32 / Back: -
+print('\033[4;33mHello, World!\033[m') #Style: 4 / Text: 33 / Back: -
+print('\033[7;34mHello, World!\033[m') #Style: 7 / Text: 34 / Back: -
+print('\033[0;35mHello, World!\033[m') #Style: 0 / Text: 35 / Back: -
+print('\033[1;36mHello, World!\033[m') #Style: 1 / Text: 36 / Back: -
+print('\033[4;37mHello, World!\033[m') #Style: 4 / Text: 37 / Back: -
+
+print('\033[0;30;40mHello, World!\033[m') #Style: 0 / Text: 30 / Back: 40
+print('\033[0;31;41mHello, World!\033[m') #Style: 0 / Text: 31 / Back: 41
+print('\033[1;32;42mHello, World!\033[m') #Style: 1 / Text: 32 / Back: 42
+print('\033[4;33;43mHello, World!\033[m') #Style: 4 / Text: 33 / Back: 43
+print('\033[0;35;44mHello, World!\033[m') #Style: 0 / Text: 35 / Back: 44
+print('\033[1;36;45mHello, World!\033[m') #Style: 1 / Text: 36 / Back: 45
+print('\033[4;37;46mHello, World!\033[m') #Style: 4 / Text: 37 / Back: 46
+print('\033[4;37;47mHello, World!\033[m') #Style: 4 / Text: 37 / Back: 47
+
